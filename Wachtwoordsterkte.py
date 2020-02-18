c819a42eeb6e45b39dd128a4bede9b998d3c5bd7
Szymon <szymon.derikx@live.com>
2020-02-18 13:53:00 +0100

    Mijn Wachtwoordsterkte code

--- Wachtwoordsterkte.py
+++ Wachtwoordsterkte.py
@@ -8,13 +8,22 @@ SpecialeTeken = 1
 txt = "Het wachtwoord moet minimaal bestaan uit {} characters, {} hoofdletter,\n {} cijfer en {} speciale teken."
 print(txt.format(Lengte, Hoofdletters, Cijfer, SpecialeTeken))
 
-
+import re
 
 x = True
 while x:
     Wachtwoord = input("\nNieuw Wachtwoord: ")
     if (len(Wachtwoord) < 8):
         print("Het opgegeven wachtwoord voldoet niet aan de minimale lengte\nProbeer het opnieuw...")
+        x = True
+    elif re.search('[0-9]', Wachtwoord) is None:
+        print("Het opgegeven wachtwoord bevat geen cijfer\nProbeer het opnieuw...")
+        x = True
+    elif re.search('[A-Z]', Wachtwoord) is None:
+        print("Het opgegeven wachtwoord heeft geen hoofdletter\nProbeer het opnieuw...")
+        x = True
+    elif re.search('[!@#$%^&*(){}_]', Wachtwoord) is None:
+        print("Het opgegeven wachtwoord bevat geen speciale teken\nProbeer het opnieuw...")
         x = True
     else:
         print("Het wachtwoord is gewijzigd!")
